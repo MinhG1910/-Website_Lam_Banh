3bd3c3ccf600574aa039ceef2248e674ec92b11b
daBonk144 <nhatnthpc07949@fpt.edu.vn>
2024-11-05 16:27:26 +0700

    Xây dựng mô hình hướng đối tượng

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WEBSITE BÁN DỤNG CỤ LÀM BÁNH</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+header('location: site/trang-chinh');
+?>
